c817ed87cbf268c5adb416f3279dded89f33ecf6
acadev98 <acatalandeveloper@gmail.com>
2024-03-05 00:07:56 -0300

    Update CorsConfiguration.java

--- src/main/java/com/acadev/teamstatsfox/config/CorsConfiguration.java
+++ src/main/java/com/acadev/teamstatsfox/config/CorsConfiguration.java
@@ -12,8 +12,31 @@ public class CorsConfiguration {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE")
-				.allowedOrigins("http://149.50.138.137:3000");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				System.out.println("##########################################cambios aplicados");
+				registry.addMapping("/**")
+                .allowedOrigins("*") // Reemplaza "*" con los orígenes permitidos de tu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+//				registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE")
+//				.allowedOrigins("http://149.50.138.137:3000");
 			}
 		};
 	}
