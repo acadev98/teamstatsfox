86851a8162d1c362bace586daddd38f7c0d83813
acadev98 <acatalandeveloper@gmail.com>
2024-03-06 15:07:29 -0300

    modificamos a develop

--- src/main/java/com/acadev/teamstatsfox/config/CorsConfiguration.java
+++ src/main/java/com/acadev/teamstatsfox/config/CorsConfiguration.java
@@ -13,12 +13,10 @@ public class CorsConfiguration {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-                .allowedOrigins("http://149.50.138.137:3000")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-//				registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE")
-//				.allowedOrigins("http://149.50.138.137:3000");
 			}
 		};
 	}
